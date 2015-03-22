61124604f8edc0b4fa8fa113c48e83215d088eef
Benjamin Nold <brnold@oakland.edu>
2015-03-21 22:02:56 -0400

    Added a pointer example
    #soIwouldn’tloosemythought
    
    #hashtagesarecool
    
    #gitcommentswithhashtagesareawesome

--- 2048Logic/2048Logic.c
+++ 2048Logic/2048Logic.c
@@ -56,3 +56,33 @@ void columnCondence(char array[], char column)
 	}
 }
 
+/* pointer example 
+retreived from http://www.tutorialspoint.com/cprogramming/c_pointer_to_an_array.htm*/
+
+#include <stdio.h>
+
+int main ()
+{
+   /* an array with 5 elements */
+   double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+   double *p;
+   int i;
+
+   p = balance;
+ 
+   /* output each array element's value */
+   printf( "Array values using pointer\n");
+   for ( i = 0; i < 5; i++ )
+   {
+       printf("*(p + %d) : %f\n",  i, *(p + i) );
+   }
+
+   printf( "Array values using balance as address\n");
+   for ( i = 0; i < 5; i++ )
+   {
+       printf("*(balance + %d) : %f\n",  i, *(balance + i) );
+   }
+ 
+   return 0;
+}
+
