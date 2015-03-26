25c38e062b1a0b9613406dea86772ac46d3a8fb0
Benjamin Nold <brnold@oakland.edu>
2015-03-25 21:11:44 -0400

    Revert ff7a2bf..9df9c08
    This rolls back to commit ff7a2bf2030f2d9087d334f4f33ec1bdf4ac88ff.

---
+++ Sources/masterHeaderFile.h
@@ -0,0 +1,5 @@
+//located in 2048Logic.c
+void arrayReduceWhiteSpaces(char array[]);
+void reduceColumnWhiteSpaces(char array[], char colNumber);
+void arrayCondence(char array[]);
+void columnCondence(char array[], char column);
